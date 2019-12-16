743caff132503bea403ae60424631fb92982c332
admin <1007942602@qq.com>
2019-12-16 11:54:05 +0800

    新增文件

---
+++ src/com/chen/utils/Pig.java
@@ -0,0 +1,11 @@
+package com.chen.utils;
+
+public class Pig extends Animal {
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
